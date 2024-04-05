b045e21d48cbda68695cd1f996cb0b1debcf5092
Strongminmin <2572737189@qq.com>
2024-04-05 17:34:17 +0800

    docs: add comment

--- packages/utils/src/node/path.ts
+++ packages/utils/src/node/path.ts
@@ -1,7 +1,10 @@
 import {dirname as _dirname} from 'node:path'
 import { fileURLToPath} from 'node:url'
 import { isBrowser } from '../env'
-
+/**
+ * @description Get the current directory path under ES Module
+ * @returns string
+ */
 export const dirname = () => {
   if (isBrowser()) {
     return ''
